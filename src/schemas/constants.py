5f890d8da761d858a614756b36cf7b73bc4066f9
Jack Linton <jrlinton@amazon.com>
2026-05-27 16:17:07 +0100

    Add Queries for all CRUD Operations to Constants File

--- src/schemas/constants.py
+++ src/schemas/constants.py
@@ -13,8 +13,28 @@ CREATE_CHANNELS_TABLE = """CREATE TABLE IF NOT EXISTS channels (
 
 GET_ALL_CHANNELS = "SELECT * FROM channels;"
 
+GET_ALL_CHANNEL_NAMES = "SELECT channel_name FROM channels;"
+
 GET_CHANNEL_DATA = "SELECT channel_name, channel_price FROM channels WHERE channel_id = %s;"
+
+ADD_CHANNEL = "INSERT INTO channels (channel_name, channel_price) VALUES (%s, %s);"
+
+UPDATE_CHANNEL = "UPDATE channels SET channel_name = %s, channel_price = %s WHERE channel_id = %s;"
+
+DELETE_CHANNEL = "DELETE FROM channels WHERE channel_id = %s;"
 
 GET_USER_PASSWORD = "SELECT password FROM users WHERE user_id = %s;"
 
 GET_USER_DATA = "SELECT username, is_admin FROM users WHERE user_id = %s;"
+
+GET_ALL_USERS = "SELECT username, is_admin FROM users;"
+
+GET_ALL_USERNAMES = "SELECT username FROM users;"
+
+ADD_USER = "INSERT INTO users (username, password, is_admin) VALUES (%s, %s, %s);"
+
+UPDATE_USER_DETAILS = "UPDATE users SET username = %s, password = %s WHERE user_id = %s;"
+
+UPDATE_USER_ADMIN_STATUS = "UPDATE users SET is_admin = %s WHERE user_id = %s;"
+
+DELETE_USER = "DELETE FROM users WHERE user_id = %s;"
