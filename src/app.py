6c8a0b53d032888cd818c4c1e727915a91aa3de8
Jack Linton <jrlinton@amazon.com>
2026-05-29 17:44:33 +0100

    Introduce Base HTML Template, Simple Index Page, and Temporary Flask Routing

--- src/app.py
+++ src/app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
+
+
+# Temporary user - remove after setting up flask-login system
+@app.context_processor
+def inject_user():
+    return {'user': {'is_authenticated': False}}
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/channels')
+def channel_browser():
+    return render_template('channel-browser.html')
+
+
+@app.route('/login')
+def login():
+    return "Under Construction"
+
+
+@app.route('/account')
+def account():
+    return "Under Construction"
 
 
 if __name__ == '__main__':
